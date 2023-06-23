bf861683d4b8bab24b9285b40cb196e8509c1977
Karla Degrano-Sebandal <130878157+karlasebandal@users.noreply.github.com>
2023-06-23 09:33:54 +0800

    updated construction

--- construction-shop/src/pages/ConstructionSite.js
+++ construction-shop/src/pages/ConstructionSite.js
@@ -1,93 +1,96 @@
-// import './App.css';
-
 import React, { useState } from 'react';
 import AddToCartButton from '../components/AddToCartButton';
 
 function ConstructionSite() {
   const [products, setProducts] = useState([
-    { id: 1, title: 'Product 1', price: 100.00},
-    { id: 2, title: 'Product 2', price: 200.00},
-    { id: 3, title: 'Product 3', price: 250.00},
-    { id: 4, title: 'Product 4', price: 250.00},
+    { id: 1, title: 'Screwdriver', price: 3010.00 },
+    { id: 2, title: 'Hammer 8', price: 2800.00 },
+    { id: 3, title: 'Rugby', price: 1250.00 },
+    { id: 4, title: 'Vulca Seal', price: 2225.00 },
   ]);
 
-  const [myCart, setMyCart] = useState([]);
-
   const renderProducts = () => {
+  
     return products.map((product) => (
-      <div key={product.id}>
-        <h2>{product.title}</h2>
+      <div key={product.id} className="product-item">
+        <h4>{product.title}</h4>
         <p>Price: ${product.price}</p>
         <AddToCartButton product={product} handleQuantityChange={handleQuantityChange} addToCart={addToCart} />
       </div>
-    ));
-  };
+  ));
 
-  const addToCart = (product) => {
-    const selectedProduct = products.find((product) => product.id === product);
-    const existingCartItem = myCart.find((item) => item.id === product);
-  
-    if (existingCartItem) {
-      // Increase the quantity
-      const updatedCart = myCart.map((item) =>
-        item.id === product ? { ...item, quantity: item.quantity + 1 } : item
-      );
-      setMyCart(updatedCart);
-    } else {
-      // Add the selected new product to 1
-      const updatedCart = [...myCart, { ...selectedProduct, quantity: 1 }];
-      setMyCart(updatedCart);
+ };
+
+const [cart, setCart] = useState([]);
+
+const handleQuantityChange = (productId, quantity) => {
+  const updatedCart = cart.map((item) => {
+    if (item.productId === productId) {
+      return { ...item, quantity: Number(quantity) };
     }
-  };
+    return item;
+  });
+  setCart(updatedCart);
+};
 
-  const handleQuantityChange = (productId, quantity) => {
-    const updatedCart = myCart.map((item) => {
-      if (item.productId === productId) {
-        return { ...item, quantity: Number(quantity) };
+const addToCart = (product) => {
+  const itemInCart = cart.find((item) => item.productId === product.id);
+  if (itemInCart) {
+    const updatedCart = cart.map((item) => {
+      if (item.productId === product.id) {
+        return { ...item, quantity: item.quantity + 1 };
       }
       return item;
     });
-    
-   //console.log(updatedCart.length)
-    setMyCart(updatedCart);
-  };
+    setCart(updatedCart);
+  } else {
+    setCart([...cart, { productId: product.id, quantity: 1 }]);
+  }
+};
 
-  const calculateTotalPrice = () => {
-    let total = 0;
-    myCart.forEach((item) => {
-      const product = products.find((p) => p.id === item.productId);
-      total += product.price * item.quantity;
-    });
-    return total.toFixed(2); //2 decimal point
-  };
+const renderCartItems = () => {
+  return cart.map((item) => {
+    const product = products.find((p) => p.id === item.productId);
+    return (
+      <div key={item.productId}>
+        <h2>{product.title}</h2>
+        <p>Price: ${product.price}</p>
+        <p>Quantity: {item.quantity}</p>
+      </div>
+    );
+  });
+};
 
-  const renderCartItems = () => {
-    return myCart.map((item) => {
-      const product = products.find((p) => p.id === item.productId);
-      return (
-        <div key={item.productId}>
-          {/* <h2>{product.title}</h2> */}
-          <p>Price: ${product.price}</p>
-          <p>Quantity: {item.quantity}</p>
-        </div>
-      );
-    });
-  };
+const calculateTotalPrice = () => {
+  let total = 0;
+  cart.forEach((item) => {
+    const product = products.find((p) => p.id === item.productId);
+    total += product.price * item.quantity;
+  });
+  return total.toFixed(2);
+};
 
   return (
-    <div>
-      <h1>My Shop</h1>
-      <div>
-        <h2>Products</h2>
-        {renderProducts()}
-      </div>
-      <div>
-        <h2>My Cart</h2>
-        {renderCartItems()}
-        <p>Total Price: {calculateTotalPrice()}</p>
-      </div>
-    </div>
-  );
-}
+  <>
+  <div >
+    <div className='layout'>
+        <h1>My Shop</h1>
 
+        <h2>Products</h2>
+        
+        {renderProducts()}
+
+        <h2>Cart</h2>
+         {renderCartItems()}
+        <p>Total Price: ${calculateTotalPrice()}</p>
+
+
+    </div>
+ 
+
+  </div>
+  </>
+  );
+  }
+  
 export default ConstructionSite;
